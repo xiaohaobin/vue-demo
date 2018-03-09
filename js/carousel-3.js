4ce20084d98379848a7d4dfb173921726e0f31c1
xiaohaobin <499970382@qq.com>
2018-03-09 19:08:41 +0800

    添加vue2.50demo版本

---
+++ js/carousel-3.js
@@ -0,0 +1,50 @@
+jQuery.fn.carousel = function(speed,time){
+	//this,指的是当前对象
+			var s = 0;//
+			var timer;
+			var d = 'right';
+			var $li = $(this);//变量三，轮播元素
+			
+			function lunbo(){
+				$li.slideUp();
+					if(d=='right'){
+						s+=1;						
+						if(s>(speed-1)){//变量一，数字有可能是5，也有可能是3
+							s=0;
+						}
+						$li.eq(s).slideDown();
+					}
+					
+					if(d=='left'){
+						s+=(-1);
+						if(s<0){
+							s=speed-1;//变量一，数字有可能是5，也有可能是3
+						}
+						$li.eq(s).slideDown();
+					}
+//				
+					
+				
+			}			
+			if(!$li.is(":animated")){
+				timer = setInterval(lunbo,time);//变量二，时间可调控
+			}
+			
+			//鼠标移动到轮播box的时候，停止轮播，移出的时候，就启动轮播
+			$(".lunboBox").hover(
+				function(){
+				clearInterval(timer);
+				},
+				function(){
+				timer = setInterval(lunbo,time);//变量二，时间可调控
+				}
+			)
+			//按键控制方向
+			$(".left").click(function(){
+				d = "left";
+			});
+			$(".right").click(function(){
+				d = "right";
+			});
+			
+}
